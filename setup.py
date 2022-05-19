824e18451fdac42db6c4713838d2fe630750a919
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2022-05-19 14:03:07 +0100

    v0.0.2: documentation added to PyPi.

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@
 import setuptools
 import unittest
 
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(name='synapi',
-    version='0.0.1',
+    version='0.0.2',
     description='Package for dealing with Synapse datasets in an easy way.',
     author='Luis Carlos Garcia Peraza Herrera',
     author_email='luiscarlos.gph@gmail.com',
@@ -15,8 +21,10 @@ setuptools.setup(name='synapi',
     package_dir={
         'synapi': 'src',
     },
-    #test_suite='tests',
     install_requires=[
       'synapseclient',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #test_suite='tests',
 )
